337f83fbb3ce797d248efb48b9331b7d3cd6cf92
Khral Steelforge <garuda2550@gmail.com>
2019-04-29 18:02:46 +0700

    Update texture-dump example header file path.
    Signed-off-by: Khral Steelforge <garuda2550@gmail.com>

--- examples/src/texture-dump.cpp
+++ examples/src/texture-dump.cpp
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <gw2formats/TextureFile.h>
 
-#include <gw2DatTools/compression/inflateTextureFileBuffer.h>
+#include <gw2dattools/compression/inflateTextureFileBuffer.h>
 
 
 namespace {
